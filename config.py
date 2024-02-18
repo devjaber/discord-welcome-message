72c70c7cf5fee9af7700d6d6b4e3efd156e96349
jaber kohistani <143834224+devjaber@users.noreply.github.com>
2024-02-18 13:13:13 +0100

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #dev-jaber-kohistani = jaberkohistani.com
 
 TOKEN = 'your-bot-token'
-CHANNEL_ID = 0000000000000000 //channel-id-here
+CHANNEL_ID = 0000000000000000 #channel-id-here
 FONT_PATH = 'BRLNSDB.TTF'
