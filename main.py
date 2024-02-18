8707d3521fe7591b67491c59c9559d90d623294e
jaber kohistani <143834224+devjaber@users.noreply.github.com>
2024-02-18 13:05:47 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import discord
+from PIL import Image, ImageDraw, ImageFont
+from config import TOKEN, CHANNEL_ID, FONT_PATH
+import requests
+
+intents = discord.Intents.default()
+intents.guilds = True
+intents.members = True
+intents.messages = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_member_join(member):
+    username = member.name
+    user_mention = member.mention
+
+    
+
+    avatar_url = member.avatar.url
+    avatar_image = Image.open(requests.get(avatar_url, stream=True).raw).convert('L') 
+
+    mask = Image.new("L", avatar_image.size, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0) + avatar_image.size, fill=255)
+    avatar_image.putalpha(mask)
+    background_image = Image.open('C:/Users/jaber/OneDrive/Desktop/io-project/py/welcome/background_image.png').convert('RGBA')
+
+
+    welcome_message = f'{username}'
+    preview_image = Image.alpha_composite(background_image, Image.new('RGBA', background_image.size, (0, 0, 0, 0)))
+
+    avatar_image = avatar_image.resize((650, 790))
+    preview_image.paste(avatar_image, (620, 65), avatar_image)
+
+    font = ImageFont.truetype(FONT_PATH, 70)
+    draw = ImageDraw.Draw(preview_image)
+    draw.text((200, 200), welcome_message, fill=(48, 48, 48), font=font)
+
+    preview_image_path = f'C:/Users/jaber/OneDrive/Desktop/io-project/user-img/{username}.png'
+    preview_image.save(preview_image_path)
+
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send(file=discord.File(preview_image_path))
+
+    send_welcome_message = f'||{user_mention}||'
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send(send_welcome_message)
+
+client.run(TOKEN)
